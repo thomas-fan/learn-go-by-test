7ca6d338878405138e793af87f124fbd7608f456
张帆 <zhangfan@azsy.cn>
2021-01-03 20:55:18 +0800

    finish iterator

--- iteration/sum.go
+++ iteration/sum.go
@@ -1,9 +1,32 @@
 package iteration
 
 // Sum add arrays
-func Sum(numbers [5]int) (sum int) {
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
 	}
+	return
+}
+
+// SumAll add to slice
+func SumAll(numbersToSum ...[]int) (sums [] int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+// SumTails add tail
+func SumTails(numbersToSumTail ...[]int) (sums [] int) {
+	for _, numbers := range numbersToSumTail {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+			continue
+		}
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
 	return
 }

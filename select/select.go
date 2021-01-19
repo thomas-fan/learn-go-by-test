9e724fe0245c1c09272f8851afadf08987b698f4
thomas <zhangfan@thomas-fan.com>
2021-01-19 10:07:14 +0800

    调整目录结构

---
+++ select/select.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	}
+}
+
+func ping(url string) chan struct{}{
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
